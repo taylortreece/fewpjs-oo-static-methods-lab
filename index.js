80b0aa550bd06f1c814abaea011c2236de395549
taylortreece <tayloratreece@gmail.com>
2021-08-06 09:52:09 -0500

    commit tree 282c2c4bbd8e98f9a075775c1aaf38a3be093a2e

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let results = []
+    let arrayOfWords = string.split(" ");
+
+    for(const word of arrayOfWords) {
+      if(exceptions.includes(word)) {
+        results.push(word)
+      } else {
+        results.push(this.capitalize(word))
+      }
+    }
+
+    return this.capitalize(results.join(" "))
+  }
+
 }
